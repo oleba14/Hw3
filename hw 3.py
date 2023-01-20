e43a8a566c68226630cc4529e4a1482e26ff8180
oleba14 <oleba14@gmail.com>
2023-01-20 14:05:10 +0100

    add hw3

---
+++ hw 3.py
@@ -0,0 +1,14 @@
+x = int(input('X: '))
+y = int(input('Y:'))
+
+if x >= 0 :
+    if y >= 0:
+        print('Перша нульового')
+    else:
+        print(' друга  нульового')
+else:
+        if y >= 0:
+            print('третя анульового')
+        else:
+         print('четверта нульового')
+
